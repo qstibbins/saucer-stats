787c09cd6586e1f898cf82f7afe08821f1c158fb
Rodrigues <rdina@audible.com>
2025-04-14 17:20:26 -0400

    Adding Schema

--- schema.sql
+++ schema.sql
@@ -3,7 +3,6 @@
 
 
 CREATE TABLE "sightings_2025" (
-    "id" INT   NOT NULL,
     "report_link" VARCHAR(255)   NOT NULL,
     "sighting_datetime" TIMESTAMP   NOT NULL,
     "city" VARCHAR(100)   NOT NULL,
@@ -14,16 +13,13 @@ CREATE TABLE "sightings_2025" (
     "reported_datetime" TIMESTAMP   NOT NULL,
     "media" VARCHAR(255)   NOT NULL,
     "explanation" TEXT   NOT NULL,
-    CONSTRAINT "pk_sightings_2025" PRIMARY KEY (
-        "id"
-     ),
+
     CONSTRAINT "uc_sightings_2025_report_link" UNIQUE (
         "report_link"
     )
 );
 
 CREATE TABLE "sightings_2024" (
-    "id" INT   NOT NULL,
     "report_link" VARCHAR(255)   NOT NULL,
     "sighting_datetime" TIMESTAMP   NOT NULL,
     "city" VARCHAR(100)   NOT NULL,
@@ -34,9 +30,7 @@ CREATE TABLE "sightings_2024" (
     "reported_datetime" TIMESTAMP   NOT NULL,
     "media" VARCHAR(255)   NOT NULL,
     "explanation" TEXT   NOT NULL,
-    CONSTRAINT "pk_sightings_2024" PRIMARY KEY (
-        "id"
-     ),
+
     CONSTRAINT "uc_sightings_2024_report_link" UNIQUE (
         "report_link"
     )
@@ -62,24 +56,13 @@ CREATE TABLE "small_dataset" (
 );
 
 CREATE TABLE "reports_by_month" (
-    "id" INT   NOT NULL,
     "sighting_year" INT   NOT NULL,
     "sighting_month" INT   NOT NULL,
-    "count" INT   NOT NULL,
-    "sighting_year_month" VARCHAR(7)   NOT NULL,
-    CONSTRAINT "pk_reports_by_month" PRIMARY KEY (
-        "id"
-     ),
-    CONSTRAINT "uc_reports_by_month_sighting_year_month" UNIQUE (
-        "sighting_year_month"
-    )
+    "count" INT   NOT NULL
 );
 
-ALTER TABLE "sightings_2024" ADD CONSTRAINT "fk_sightings_2024_id" FOREIGN KEY("id")
-REFERENCES "sightings_2025" ("id");
-
-ALTER TABLE "small_dataset" ADD CONSTRAINT "fk_small_dataset_id" FOREIGN KEY("id")
-REFERENCES "reports_by_month" ("id");
+ALTER TABLE "sightings_2024" ADD CONSTRAINT "fk_sightings_2024_report_link" FOREIGN KEY("report_link")
+REFERENCES "sightings_2025" ("report_link");
 
 ALTER TABLE "small_dataset" ADD CONSTRAINT "fk_small_dataset_report_link" FOREIGN KEY("report_link")
 REFERENCES "sightings_2024" ("report_link");
